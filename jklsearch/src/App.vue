2d6e09edb4341e5d175b5248e2fd1425b4990423
Liwen <you@example.com>
2022-09-09 12:52:41 +1000

    some try

--- jklsearch/src/App.vue
+++ jklsearch/src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-    <div class="card-body">
-      <h2 class="card-title">
-        Shoes!
-        <div class="badge badge-secondary">NEW</div>
-      </h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <div class="badge badge-outline">Fashion</div> 
-        <div class="badge badge-outline">Products</div>
+  <link rel="stylesheet" type="text/css" href="index.css">
+
+    <div id= "search_box">
+      <p id = "logo">
+        JKL.IO
+      </p>
+      <div id="input_text_framework">
+        <input type="text" placeholder="Type here" class="input input-ghost w-full max-w-xs" id="input_textarea"/>
       </div>
     </div>
+
+  <div id="result_box">
+    <div class = "results">
+      <a href="" class = "result_link"> link </a><br>
+      <a href="" class = "result_title"> title 1 </a>
+      <p class = "result_des">description</p>
+    </div>
+    <br>
+
+    <div class = "results">
+      <a href="" class = "result_link"> link </a><br>
+      <a href="" class = "result_title"> title 2 </a>
+      <p class = "result_des">description</p>
+    </div>
+    <br>
+
+    <div class = "results">
+    <a href="" class = "result_link"> link </a><br>
+    <a href="" class = "result_title"> title 3 </a>
+    <p class = "result_des">description</p>
+    </div>
+    <br>
+
+    <div class = "results">
+    <a href="" class = "result_link"> link </a><br>
+    <a href="" class = "result_title"> title 4 </a>
+    <p class = "result_des">description</p>
+    </div>
+    <br>
+
   </div>
+
 </template>
 
