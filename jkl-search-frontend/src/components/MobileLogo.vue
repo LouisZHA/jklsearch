cdcd2e840083f0f669b331e169a675cfcdf0cd1b
Suowei Hu <suowei.h@gmail.com>
2022-09-11 15:15:39 +1000

    Compartmentalized into vue-components

---
+++ jkl-search-frontend/src/components/MobileLogo.vue
@@ -0,0 +1,12 @@
+.<template>
+  	  <!-- LOGO -->
+		<div id="logo-box" class="absolute top-20 left-3 w-auto h-14 ml-28 mr-28 rounded-md ">
+		<div class="w-full h-full text-center font-semibold text-5xl text-light-text">JKL.io</div>
+	  </div><div class="w-auto h-6 text-right ml-0  mr-0  rounded-md"></div>
+</template>
+
+<script>
+export default {
+	name:"MobileLogo"
+}
+</script>
