8376c951099143c771bc94a5c6cb5f35024471f1
ScottBoomxx <scottchanboom@gmail.com>
2022-09-12 15:17:55 +1000

    version

---
+++ src 2/App.vue
@@ -0,0 +1,43 @@
+<template>
+  
+  <router-view/>
+  
+
+</template>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+/*flex布局类名*/ 
+
+.display-float{
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+}
+.flex{
+  display: flex;
+  align-items: center;
+
+}
+
+.page_content{
+  box-sizing: border-box;
+  display: block;
+  width:100%;
+  padding:20px;
+  background: #ffff;
+  margin-top:30px;
+
+}
+
+
+</style>
